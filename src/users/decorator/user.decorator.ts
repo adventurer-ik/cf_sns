889efa19e5797f57464674cb03539f0e2d2706e4
ik <adventurer-ik@daum.net>
2024-02-19 22:30:06 +0900

    18-3. custom decorator - data param 사용

--- src/users/decorator/user.decorator.ts
+++ src/users/decorator/user.decorator.ts
@@ -3,18 +3,25 @@ import {
   InternalServerErrorException,
   createParamDecorator,
 } from '@nestjs/common';
+import { UsersModel } from '../entities/users.entity';
 
-export const User = createParamDecorator((data, context: ExecutionContext) => {
-  const req = context.switchToHttp().getRequest();
-  const user = req.user;
+export const User = createParamDecorator(
+  (data: keyof UsersModel | undefined, context: ExecutionContext) => {
+    const req = context.switchToHttp().getRequest();
+    const user = req.user as UsersModel;
 
-  // accessTokenGuard와 같이 쓰기 때문에 절대 에러가 발생하지 않겠지만,
-  // 혹시나 다른 개발자가 사양을 모르고 accessTokenGuard와 같이 안쓰는 경우를 대비 함.
-  if (!user) {
-    throw new InternalServerErrorException(
-      'Request에 user property가 존재하지 않습니다. User Decorator는 AccessTokenGuard와 함께 사용해야 합니다.',
-    );
-  }
+    // accessTokenGuard와 같이 쓰기 때문에 절대 에러가 발생하지 않겠지만,
+    // 혹시나 다른 개발자가 사양을 모르고 accessTokenGuard와 같이 안쓰는 경우를 대비 함.
+    if (!user) {
+      throw new InternalServerErrorException(
+        'Request에 user property가 존재하지 않습니다. User Decorator는 AccessTokenGuard와 함께 사용해야 합니다.',
+      );
+    }
 
-  return user;
-});
+    if (data) {
+      return user[data];
+    }
+
+    return user;
+  },
+);
