6b1e608f88087be17d69cf73b46227a31749bc5d
ik <adventurer-ik@daum.net>
2024-03-03 22:20:19 +0900

    21-1 Exclude Annotation 21-2 Exclude Annotation 옵션 2개 학습

--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -6,6 +6,7 @@ import { IsEmail, IsString, Length } from 'class-validator';
 import { lengthValidationMessages } from 'src/common/validation-message/length-validation.message';
 import { stringValidationMessage } from 'src/common/validation-message/string-validation.message';
 import { emailValidationMessage } from 'src/common/validation-message/email-validation.message';
+import { Exclude } from 'class-transformer';
 
 @Entity()
 export class UsersModel extends BaseModel {
@@ -40,6 +41,26 @@ export class UsersModel extends BaseModel {
   @IsString({ message: stringValidationMessage })
   @Length(4, 20, {
     message: lengthValidationMessages,
+  })
+  /**
+   * Request
+   * frontend -> backend
+   * plain object (JSON) -> class instance (dto)
+   *
+   * Response
+   * backend -> frontedn
+   * class instance (dto) -> plain object (JSON)
+   *
+   * toClassOnly -> class instance (dto) 변환 될 때만: 요청(Request) 보낼때만 적용!
+   * toPlainOnly -> plain object (JSON) 변환 될 때만: 응답(Response) 할 때만 적용!
+   *
+   * 여기서 password는 우리가 사용자로부터 입력을 받고 확인을 해야 하므로 (서버측)은 필요함.
+   * 다만, 응답할 때는 알려줘서는 안되는 데이터.
+   * 그러므로, 응답시에는 제외하도록 해야 함.
+   * toPlainOnly를 true하면, 응답할 때만 exclude 하게 됨.
+   */
+  @Exclude({
+    toPlainOnly: true,
   })
   password: string;
 
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  Post,
+  UseInterceptors,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -6,6 +13,14 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
+  /**
+   * serialization -> 직렬화
+   *  : 현재 시스템(Nest.js)에서 사용되는 데이터의 구조 / 포맷을 다른 시스템에서도 쉽게 사용 할 수 있는 포맷으로 변환
+   *  : class object에서 JSON 포맷으로 변환
+   *
+   * deserialization -> 역직렬화
+   */
+  @UseInterceptors(ClassSerializerInterceptor)
   getUsers() {
     return this.usersService.getAllUser();
   }
