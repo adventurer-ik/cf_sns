ea0f8de7d1400daf5252d374178003b03859489c
ik <adventurer-ik@daum.net>
2024-05-02 21:41:45 +0900

    24-9. DTO 프로퍼티 whitelisting 하기

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,13 @@ async function bootstrap() {
       transformOptions: {
         enableImplicitConversion: true,
       },
+      // 유효성 검사 수행시, DTO에 정의되지 않은 속성을 자동으로 제거 함.
+      // 이는 클라이언트로부터 예상치 못한 필드가 서버로 유입되는 것을 방지해줌
+      whitelist: true,
+      // whitelist의 옵션을 확장해주는 개념
+      // DTO에 정의되지 않은 필드가 있을 경우, 단순히 제거 하는 것이 아니라 요청 자체를 거부하고 예외 발생시킴.
+      // 개발자가 바로 알아차리고 처리할 수 있도록 도와주며, 보안 및 데이터 무결성 측면에서 강력함.
+      forbidNonWhitelisted: true,
     }),
   );
 
