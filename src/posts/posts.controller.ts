855cf4e927257918790b6b8360d3306d50f486b3
ik <adventurer-ik@daum.net>
2024-06-06 16:36:54 +0900

    30-4. QueryRunner 커스텀 데코레이터 만들기 & Transaction Interceptor 적용하기

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Delete,
   Get,
+  InternalServerErrorException,
   Param,
   ParseIntPipe,
   Patch,
@@ -19,15 +20,16 @@ import { UpdatePostDto } from './dto/update-post.dto';
 import { paginatePostDto } from './dto/paginate-post.dto';
 import { UsersModel } from 'src/users/entities/users.entity';
 import { ImageModelType } from 'src/common/entity/image.entity';
-import { DataSource } from 'typeorm';
+import { DataSource, QueryRunner as QR } from 'typeorm';
 import { PostImagesService } from './image/images.service';
 import { LogInterceptor } from 'src/common/interceptor/log.interceptor';
+import { TransactionInterceptor } from 'src/common/interceptor/transaction.interceptor';
+import { QueryRunner } from 'src/common/decorator/query-runner.decorator';
 
 @Controller('posts')
 export class PostsController {
   constructor(
     private readonly postsService: PostsService,
-    private readonly dataSource: DataSource,
     private readonly postImagesService: PostImagesService,
   ) {}
 
@@ -77,51 +79,31 @@ export class PostsController {
 
   @Post()
   @UseGuards(AccessTokenGuard)
-  async postPosts(@User('id') userId: number, @Body() body: CreatePostDto) {
-    // 트랜잭션과 관련된 모든 쿼리를 담당
-    // 쿼리 러너 생성한다.
-    const qr = this.dataSource.createQueryRunner();
+  @UseInterceptors(TransactionInterceptor)
+  async postPosts(
+    @User('id') userId: number,
+    @Body() body: CreatePostDto,
+    @QueryRunner() qr: QR,
+  ) {
+    // transaction과 묶이지 않은 작업들은 트랜잭션이 정상적으로 완료되면, 마지막에 해주는 것이 좋음
+    // 트랜잭션 도중 문제가 생기면 언제든지 돌리면 되나, 관련없는 것들은 번거로워짐.
+    const post = await this.postsService.createPost(userId, body, qr);
 
-    // 쿼리 러너에 연결한다.
-    await qr.connect();
-    // 쿼리 러너에서 트랜잭션을 시작한다.
-    // 이 시점부터 같은 쿼리 러너를 사용하면 트랜잭션 안에서 DB 액션을 실행 할 수 있음.
-    // -> 모든 쿼리를 한번에 묶어주는 역할을 queryRunner가 해준다고 생각하면 됨.
-    await qr.startTransaction();
+    // 에러 테스트용
+    // throw new InternalServerErrorException('삐용 삐용 테스트용 에러 발생');
 
-    // 로직 실행
-    try {
-      // transaction과 묶이지 않은 작업들은 트랜잭션이 정상적으로 완료되면, 마지막에 해주는 것이 좋음
-      // 트랜잭션 도중 문제가 생기면 언제든지 돌리면 되나, 관련없는 것들은 번거로워짐.
-      const post = await this.postsService.createPost(userId, body, qr);
-
-      // throw new InternalServerErrorException('삐용 삐용 테스트용 에러 발생');
-
-      for (let i = 0; i < body.images.length; i++) {
-        await this.postImagesService.createPostImage(
-          {
-            post,
-            order: i,
-            path: body.images[i],
-            type: ImageModelType.POST_IMAGE,
-          },
-          qr,
-        );
-      }
-
-      // 쿼리 모두 정상적으로 수행되었을 경우, 저장!
-      // 관련 기능들이 전부 동시에 실행되면서 DB에 적용이 됨.
-      await qr.commitTransaction();
-      // release 해줘야 함. -> queryRunner 작업 종료
-      await qr.release();
-
-      return this.postsService.getPostById(post.id);
-    } catch (error) {
-      // 어떤 에러든 에러가 던져지면, 또는 발생하면 트랜잭션 종료하고 원래 상태로 돌린다. (롤백)
-      await qr.rollbackTransaction();
-      // query runner 해제 해줌 (더이상 이 queryRunner 안씀...)
-      await qr.release();
+    for (let i = 0; i < body.images.length; i++) {
+      await this.postImagesService.createPostImage(
+        {
+          post,
+          order: i,
+          path: body.images[i],
+          type: ImageModelType.POST_IMAGE,
+        },
+        qr,
+      );
     }
+    return this.postsService.getPostById(post.id, qr);
   }
 
   @Patch(':id')
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -168,8 +168,9 @@ export class PostsService {
     };
   }
 
-  async getPostById(id: number) {
-    const post = await this.postsRepository.findOne({
+  async getPostById(id: number, qr?: QueryRunner) {
+    const repository = this.getRepository(qr);
+    const post = await repository.findOne({
       where: { id },
       // relations: ['author', 'images'],
       ...DEFAULT_POST_FIND_OPTION,
