4a94446b977590b04ab7b1afc4149acc56e7c4f5
ik <adventurer-ik@daum.net>
2024-07-18 21:43:57 +0900

    34-1. Validation Pipe

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -13,6 +13,7 @@ import { ChatsService } from './chats.service';
 import { EnterChatDto } from './dto/enter-chat.dto';
 import { CreateMessageDto } from './messages/dto/create-messages.dto';
 import { ChatsMessagesService } from './messages/messages.service';
+import { UsePipes, ValidationPipe } from '@nestjs/common';
 
 // socket.io 가 연결하는 곳을 우리는, nest.js에서는 gateway라고 부름.
 @WebSocketGateway({
@@ -38,6 +39,22 @@ export class ChatsGateway implements OnGatewayConnection {
     );
   }
 
+  @UsePipes(
+    new ValidationPipe({
+      // 만약, 값이 없을 경우 실제 DTO에 명시된 디폴트 값을 넣은채로 인스턴스 생성하도록 허가.
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      // 유효성 검사 수행시, DTO에 정의되지 않은 속성을 자동으로 제거 함.
+      // 이는 클라이언트로부터 예상치 못한 필드가 서버로 유입되는 것을 방지해줌
+      whitelist: true,
+      // whitelist의 옵션을 확장해주는 개념
+      // DTO에 정의되지 않은 필드가 있을 경우, 단순히 제거 하는 것이 아니라 요청 자체를 거부하고 예외 발생시킴.
+      // 개발자가 바로 알아차리고 처리할 수 있도록 도와주며, 보안 및 데이터 무결성 측면에서 강력함.
+      forbidNonWhitelisted: true,
+    }),
+  )
   @SubscribeMessage('create_chat')
   async createChat(
     @MessageBody() data: CreateChatDto,
